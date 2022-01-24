ccff64471dc2dff6aca1796c51d43c3d7b1b69dc
Himmat Khalsa <himmatsinghkhalsa@gmail.com>
2022-01-23 20:54:02 -0700

    implement PUT requests

--- queries.js
+++ queries.js
@@ -36,16 +36,15 @@ module.exports.getReviews = ({page = 0, count = 5, sort, product_id = 1}) => {
   } else if (sort === 'newest') {
     order = 'date DESC';
   }
+  //TODO: skip review if reported = t
   return pool
     .query(`SELECT * FROM reviews WHERE product_id = ${product_id} ORDER BY ${order} LIMIT ${count} OFFSET ${page};`)
 }
 
 module.exports.getPhotos = (review_id) => {
-  // console.log('review ID: ', review_id);
   return pool
     .query(`SELECT id, url FROM reviews_photos WHERE review_id = ${review_id};`)
     .then(photos => {
-      // console.log('PHOTOS QUERY: ', photos);
       return photos.rows;
     })
 }
@@ -58,5 +57,39 @@ module.exports.getReviewsMeta = ({product_id = 1}) => {
   //characteristics
   return pool
     .query(`SELECT * FROM reviews WHERE product_id = ${product_id};`)
+  //need reponse to look something like this:  {
+  //   product_id: '37312',
+  //   ratings: { '2': '1', '3': '5', '4': '2', '5': '3' },
+  //   recommended: { false: '2', true: '9' },
+  //   characteristics: { Quality: { id: 125035, value: '4.0000000000000000' } }
+  //   }
 }
 
+module.exports.postReview = ({
+  product_id,
+  rating,
+  summary,
+  body,
+  recommend,
+  name,
+  email,
+  photos, //maybe photos go to a separate query
+  characteristics //maybe these go to a separate query also
+}) => {
+  return pool
+    .query(`INSERT INTO reviews...`) //need to add review data to 'review' table as well
+    //also insert data into 'reviews_photos' if there are photos
+    //also insert score for each characteristic in 'characteristic_reviews'
+}
+
+module.exports.markReviewHelpful = (review_id) => {
+  review_id = 2; //use valid product ID while still using sample data
+  return pool
+    .query(`UPDATE reviews SET helpfulness = helpfulness+1 WHERE id=${review_id}`)
+}
+
+module.exports.reportReview = (review_id) => {
+  review_id = 2; //use valid product ID while still using sample data
+  return pool
+    .query(`UPDATE reviews SET reported = 't' WHERE id=${review_id}`)
+}
