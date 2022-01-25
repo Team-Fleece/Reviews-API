f7dbffed5f0e075d6affaa21d528bcc09f3ee8b5
Himmat Khalsa <himmatsinghkhalsa@gmail.com>
2022-01-24 18:03:06 -0700

    save changes to queries

---
+++ queries.js
@@ -0,0 +1,61 @@
+const { Pool, Client } = require('pg');
+
+const pool = new Pool({
+  user: 'himmat',
+  host: 'localhost',
+  database: 'mydb',
+  password: 'boardtoadsilversock',
+  port: 5432,
+  // idleTimeoutMillis: 0,
+  // connectionTimeoutMillis: 0,
+})
+
+const sortHelpful = (reviewList) => {
+  return reviewList.sort((a, b) => b.helpfulness - a.helpfulness);
+}
+
+const sortNewest = (reviewList) => {
+  return reviewList.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
+}
+
+//bases sort on combination of helpfulness and newness, valuing each helpful count at Math.pow(10, 8)
+const sortRelevant = (reviewList) => {
+  return reviewList.sort((a, b) => {
+    return
+      (Date.parse(b.date)/Math.pow(10, 8) + b.helpfulness)
+      -
+      (Date.parse(a.date)/Math.pow(10, 8) + a.helpfulness);
+  })
+}
+
+module.exports.getReviews = ({page = 0, count = 5, sort, product_id = 1}) => {
+  product_id = 2;
+  let order = 'helpfulness DESC, date DESC';
+  if (sort === 'helpful') {
+    order = 'helpfulness DESC';
+  } else if (sort === 'newest') {
+    order = 'date DESC';
+  }
+  return pool
+    .query(`SELECT * FROM reviews WHERE product_id = ${product_id} ORDER BY ${order} LIMIT ${count} OFFSET ${page};`)
+}
+
+module.exports.getPhotos = (review_id) => {
+  // console.log('review ID: ', review_id);
+  return pool
+    .query(`SELECT id, url FROM reviews_photos WHERE review_id = ${review_id};`)
+    .then(photos => {
+      // console.log('PHOTOS QUERY: ', photos);
+      return photos.rows;
+    })
+}
+
+module.exports.getReviewsMeta = ({product_id = 1}) => {
+  //product_id from reviews
+  //ratings added up from each review
+  //recommend added up from each review
+  //characteristics
+  return pool
+    .query(`SELECT * FROM reviews WHERE product_id = ${product_id};`)
+}
+
